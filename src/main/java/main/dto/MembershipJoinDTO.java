538fb068a65a1dacf1c6cc7dc9db14f5705e5f09
PerryCameron <pcameron28@yahoo.com>
2021-08-15 12:30:26 -0400

    changed dto

--- src/main/java/main/dto/MembershipJoinDTO.java
+++ src/main/java/main/dto/MembershipJoinDTO.java
@@ -2,32 +2,17 @@ package main.dto;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import main.model.Membership;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class MembershipJoinDTO implements Serializable {
 	
-	private Membership membership;
+	int msid;
+	int pid;
+	Date joinDate;
+	String lastName;
+	String firstName;
+	String address;
+	String city;
+	String state;
+	String zip;
 	
 }
