6f19f94680b48ec214bf87125d4147280f9fcaaf
PerryCameron <pcameron28@yahoo.com>
2021-08-15 11:44:58 -0400

    testing

--- src/main/java/main/controller/MembershipListController.java
+++ src/main/java/main/controller/MembershipListController.java
@@ -1,5 +1,20 @@
 package main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import main.repository.MembershipRepository;
+import main.repository.PersonRepository;
+
+@RestController
 public class MembershipListController {
+	
+	@Autowired
+	private MembershipRepository membershipRepository;
+	
+	@Autowired
+	private PersonRepository personRepository;
+	
+	
 
 }
