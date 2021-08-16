d7c87804e35d2045fa2c2330fa436bbb0307a8a8
PerryCameron <pcameron28@yahoo.com>
2021-08-16 12:52:22 -0400

    testing git on visual studio code

--- src/main/java/main/HalyardWebApplication.java
+++ src/main/java/main/HalyardWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HalyardWebApplication {
-
+// main application
 	public static void main(String[] args) {
 		SpringApplication.run(HalyardWebApplication.class, args);
 	}
