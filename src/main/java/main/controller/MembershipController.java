47ad5c25017fe8484063b0f6010c0856d24f6ac0
PerryCameron <pcameron28@yahoo.com>
2021-08-23 15:43:35 -0400

    abiliy to use request parameters

--- src/main/java/main/controller/MembershipController.java
+++ src/main/java/main/controller/MembershipController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import main.dto.MembershipDTO;
@@ -41,7 +42,13 @@ public class MembershipController {
 		model.addAttribute("membershipIdDTO", membershipIdDTO);
 		return "memberships";
 	}
-	
+
+	@GetMapping("membership")
+	public String getAllMembershipsByYearSorted(Model model, @RequestParam int year, @RequestParam String sort) {
+		List<MembershipIdDTO> membershipIdDTO = membershipService.findMembershipIdEntityByFiscalYear(year, sort);
+		model.addAttribute("membershipIdDTO", membershipIdDTO);
+		return "memberships";
+	}
 
 	@GetMapping("/membershipidjson")
 	@ResponseBody
